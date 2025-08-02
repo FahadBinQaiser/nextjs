023bbf598086cf25c939347ecc6d08ab1e3d8b52
Fahad <fahadbinqaiser3@gmail.com>
2025-08-02 14:46:14 +0500

    Day 3 done

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,15 @@
+import Head from "next/head";
 export default function About() {
-  return <h1>About Page</h1>;
+  return (
+    <>
+      <Head>
+        <title>About Us</title>
+        <meta
+          name="description"
+          content="Learn more about our company and team."
+        />
+      </Head>
+      <h1>About Page</h1>
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold text-center my-10">
-        Welcome to the Home Page
-      </h1>
-    </div>
+    <>
+      <Head>
+        <title>Next JS</title>
+        <meta name="description" content="Learning Next.js." />
+      </Head>
+      <div>
+        <h1 className="text-4xl font-bold text-center my-10">
+          Welcome to the Home Page
+        </h1>
+      </div>
+    </>
   );
 }
